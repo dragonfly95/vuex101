77cd22cd4761b08ae75a9bd06b695c438243808f
dragonfly95 <dQzNKCyAgf43e3B>
2021-10-25 12:10:53 +0900

    mm

---
+++ src/components/common/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1><a href="#" class="logo"><img alt="Vue logo" src="../../assets/logo.png" width="80"></a></h1>
+    <div class="menuWrap">
+      <ul class="menu">
+        <li><a href="javascript:;">메뉴1</a></li>
+        <li><a href="javascript:;">메뉴2</a></li>
+        <li><a href="javascript:;">메뉴3</a></li>
+        <li><a href="javascript:;">메뉴4</a></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
